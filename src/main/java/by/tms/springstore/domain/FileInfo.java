aa8c0c7f2d06d0028a427070ab7d46a932811309
Viktar Rusakovich <mrisviz.rus@gmail.com>
2023-06-23 01:28:17 +0300

    <!--закомментил часть тестового кода -->

--- src/main/java/by/tms/springstore/domain/FileInfo.java
+++ src/main/java/by/tms/springstore/domain/FileInfo.java
@@ -1,27 +1,28 @@
-package by.tms.springstore.domain;
-
-public class FileInfo {
-    private String filename;
-    private String filepath;
-
-    public FileInfo(String filename, String filepath) {
-        this.filename = filename;
-        this.filepath = filepath;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public String getFilepath() {
-        return filepath;
-    }
-
-    public void setFilepath(String filepath) {
-        this.filepath = filepath;
-    }
-}
+////<!--Тестовый период, не обращать внимание-->
+//package by.tms.springstore.domain;
+//
+//public class FileInfo {
+//    private String filename;
+//    private String filepath;
+//
+//    public FileInfo(String filename, String filepath) {
+//        this.filename = filename;
+//        this.filepath = filepath;
+//    }
+//
+//    public String getFilename() {
+//        return filename;
+//    }
+//
+//    public void setFilename(String filename) {
+//        this.filename = filename;
+//    }
+//
+//    public String getFilepath() {
+//        return filepath;
+//    }
+//
+//    public void setFilepath(String filepath) {
+//        this.filepath = filepath;
+//    }
+//}
